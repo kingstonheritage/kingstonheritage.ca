be48437bf10797ea8e47eb95c28046dcfe088699
Cameron Hurd <me@cameronhurd.com>
2020-02-12 22:54:54 -0500

    update to https tho

--- wp-content/plugins/widgetkit-1/cache/widgetkit-740bddb5.js
+++ wp-content/plugins/widgetkit-1/cache/widgetkit-740bddb5.js
@@ -1,1 +1,1 @@
-var widgetkitajax="http://kingstonheritage.ca/wp-admin/admin-ajax.php?action=widgetkit&ajax=1";
+var widgetkitajax="https://kingstonheritage.ca/wp-admin/admin-ajax.php?action=widgetkit&ajax=1";
